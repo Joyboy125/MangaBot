fd0c5ff58f0481d147981c4cea9464f19f77ed98
joy boy <156354499+Joyboy125@users.noreply.github.com>
2024-11-18 18:35:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 env_vars = {
   # Get From my.telegram.org
-  "API_HASH": "",
+  "API_HASH": "f757ca005f4a320bcea5ced947bbff5e",
   # Get From my.telegram.org
-  "API_ID": "",
+  "API_ID": "27011028",
   #Get For @BotFather
-  "BOT_TOKEN": "",
+  "BOT_TOKEN": "7505997121:AAF2Wf6naoMWdEJ1VVWgDHQNgf48Jsv6_DE",
   # Get For tembo.io
-  "DATABASE_URL_PRIMARY": "",
+  "DATABASE_URL_PRIMARY": "postgresql://postgres:k0edUULYD0G6vZy0@fruitfully-legitimate-lemur.data-1.use1.tembo.io:5432/postgres",
   # Logs Channel Username Without @
   "CACHE_CHANNEL": "",
   # Force Subs Channel username without @
@@ -14,13 +14,12 @@ env_vars = {
   # {chap_num}: Chapter Number
   # {chap_name} : Manga Name
   # Ex : Chapter {chap_num} {chap_name} @Manhwa_Arena
-  "FNAME": "",
+  "FNAME": "{chap_num}. {chap_name}",
   # Put Thumb Link 
-  "THUMB": ""
+  "THUMB": "thumbnail.jpg"
 }
 
 dbname = env_vars.get('DATABASE_URL_PRIMARY') or env_vars.get('DATABASE_URL') or 'sqlite:///test.db'
 
 if dbname.startswith('postgres://'):
     dbname = dbname.replace('postgres://', 'postgresql://', 1)
-    
